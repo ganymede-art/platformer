81d1c15534d0d17e605f0ef9062a5f90b79e665d
ganymede-art <89941188+ganymede-art@users.noreply.github.com>
2021-09-06 21:38:18 +0100

    Add files via upload

---
+++ map/MapAutoMusicController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapAutoMusicController : MonoBehaviour
+{
+    GameMasterController master;
+    public string music_name = string.Empty;
+    public bool music_is_loop = true;
+
+    void Start()
+    {
+        master = GameMasterController.GetMasterController();
+        master.audio_controller.PlayMusic(music_name, music_is_loop);
+        GameObject.Destroy(this);
+    }
+}
